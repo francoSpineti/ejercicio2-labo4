ad3834b599071ff117db903c6e1dd87897cfd14a
franco <franco.spineti@gmail.com>
2022-08-30 18:29:40 -0300

    first commit

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const app = express();
 const path = require('path');
 
 
-app.use(express.static(__dirname + '/angularapp'));
+app.use(express.static(__dirname + '/angular'));
 app.listen(process.env.PORT || 8080);
 app.get('/*', function(req, res) {
-res.sendFile(path.join(__dirname + '/angularapp/index.html'));
+res.sendFile(path.join(__dirname + '/angular/index.html'));
 });
 console.log('Build successful!!');

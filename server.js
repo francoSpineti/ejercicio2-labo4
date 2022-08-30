641bc8c2bb57f1e6954f4efc8562072ec82cdcbe
franco <franco.spineti@gmail.com>
2022-08-30 18:47:39 -0300

    first commit

--- server.js
+++ server.js
@@ -7,4 +7,6 @@ app.listen(process.env.PORT || 8080);
 app.get('/*', function(req, res) {
 res.sendFile(path.join(__dirname + '/dist/ejercicio-2/index.html'));
 });
+
+console.log('rutaaaaa***************: ' + __dirname);
 console.log('Build successful!!');
